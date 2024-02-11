30eef5dd949c0dd2a0b573c4ce4cba252e20ae52
Feisal Ahmed <feisala132@gmail.com>
2024-02-11 22:43:30 +0000

    Adding Chapter 1 exercises for programmers files

---
+++ CH1/scripts.js
@@ -0,0 +1,106 @@
+
+// first solution 
+
+// let billAmount = 0
+// let tip = 0
+// let tipRate = 0
+// let total = 0
+
+
+// const billAmountstr = prompt('What is the bill amount?')
+// const tipRatestr = prompt('What is the tip rate?')
+
+// billAmount = Number(billAmountstr)
+// tipRate = Number(tipRatestr)
+// tip = billAmount * (tipRate / 100)
+// tip = Math.round(tip * 100) / 100
+// total = billAmount + tip
+
+// console.log(`Tip: $${tip}`)
+// console.log(`Total: $${total}`)
+
+
+
+// challenge 1
+
+// let billAmount = 0
+// let tip = 0
+// let tipRate = 0
+// let total = 0
+// challenge 2
+// const billAmountstr = prompt('What is the bill amount?')
+// const tipRatestr = prompt('What is the tip rate?')
+
+// billAmount = Number(billAmountstr)
+// tipRate = Number(tipRatestr)
+
+// tip = billAmount * (tipRate / 100)
+// tip = Math.round(tip * 100) / 100
+// total = billAmount + tip
+
+// if (isNaN(billAmount)){
+//     console.log('Please enter a valid number for the bill amount.')
+//   }
+// else if (isNaN(tipRate)) {
+//     console.log('Please enter a valid number for the tip rate.')
+// }
+
+// else {
+//     console.log(`Tip: $${tip}`)
+//     console.log(`Total: $${total}`)    
+// }
+
+// challenge 2 and challenge 3 
+
+// let billAmount = 0
+// let tip = 0
+// let tipRate = 0
+// let total = 0
+
+
+// let billAmountstr = prompt('What is the bill amount?')
+
+// let tipRatestr = prompt('What is the tip rate?')
+
+// while(isNaN(Number(billAmountstr)) || Number(billAmountstr) < 0){
+//     billAmountstr = prompt('Enter a positive number for the bill amount')
+// }
+// while(isNaN(Number(tipRatestr)) || Number(tipRatestr) < 0){
+//     tipRatestr = prompt('Enter a positive number for the tip rate')
+// }
+
+// billAmount = Number(billAmountstr)
+// tipRate = Number(tipRatestr)
+// tip = billAmount * (tipRate / 100)
+// tip = Math.round(tip * 100) / 100
+// total = billAmount + tip
+
+
+// console.log(`Tip: $${tip}0`)
+// console.log(`Total: $${total}0`)
+
+
+
+
+// Challenge 4 
+
+function askBillamount () {
+    let billAmountstr = prompt('What is the bill amount?')
+    return billAmountstr
+}
+
+function askTiprate () {
+    let tipRatestr = prompt('What is the tip rate?')
+    return tipRatestr
+}
+
+
+
+
+function checkBillamount (askBillamount) {
+while(isNaN(Number(askBillamount())) || Number(askBillamount()) < 0){
+    billAmountstr = prompt('Enter a positive number for the bill amount')
+}
+}
+
+checkBillamount()
