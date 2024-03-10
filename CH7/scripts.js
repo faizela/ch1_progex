4334042e1de183f8572d79bf9b002d394007eb9a
Feisal Ahmed <feisala132@gmail.com>
2024-03-10 19:41:14 +0000

    Adding ch2 index, scripts files

---
+++ CH7/scripts.js
@@ -0,0 +1,33 @@
+// Exercise 1
+
+// const answers = ['Yes', 'No', 'Maybe', 'Ask again later']
+
+// const random_choice = Math.floor(Math.random() * answers.length)
+
+
+// prompt('What\'s your question? ')
+// alert(answers[random_choice])
+
+// Exercise 2
+
+const employees = [
+'John Smith',
+'Jackie Jackson',
+'Chris Jones',
+'Amanda Cullen',
+'Jeremy Goodwin'
+]
+console.log(`There are ${employees.length} employees:`)
+for (let employee of employees) {
+    console.log(employee)
+}
+
+// employee = prompt('Enter an employee name to remove: ')
+
+// if (employees.includes(employee)) {
+    
+// }
+
+
+
+
